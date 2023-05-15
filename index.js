f5fe03d1bcbf5dc4ec148b9560ee8f17a0322ce7
K.S <del12dmc@gmail.com>
2023-05-16 00:25:41 +0300

    corrected as per eslint

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-function InputField(id){
+class InputField {
+  constructor(id) {
     this.element = document.createElement('input');
     this.element.setAttribute('type', 'text');
     this.element.setAttribute('id', id);
-    this.element.setAttribute('placeholder', "your input here");
-    this.element.setAttribute("style", "margin: 10px");
-}
+    this.element.setAttribute('placeholder', 'your input here');
+    this.element.setAttribute('style', 'margin: 10px');
+  }
 
-InputField.prototype.setValue = function(value) {
+  setValue(value) {
     this.element.value = value;
-};
+  }
 
-InputField.prototype.isEmpty = function() {
-    const value = this.element.value;
+  isEmpty() {
+    const { value } = this.element;
     return value.trim() === '';
-};
+  }
 
-InputField.prototype.isPhone = function() {
-    const value = this.element.value;
+  isPhone() {
+    const { value } = this.element;
     const phoneNumber = value.replace(/\D/g, '');
-    return phoneNumber.length === 10; 
-};      
+    return phoneNumber.length === 10;
+  }
 
-InputField.prototype.addToElement = function(elementId){
+  addToElement(elementId) {
     const parentElement = document.getElementById(elementId);
     parentElement.appendChild(this.element);
-};
-
+  }
+}
 
 const field = new InputField('myIdNo1');
 const field2 = new InputField('myIdNo2');
 const field3 = new InputField('myIdNo3');
 
-field.addToElement("first");
-field2.addToElement("second");
-field3.addToElement("second");
-
-console.log(field.value);
-field.setValue("54");
-
-console.log(field.isEmpty());
-field.setValue("0541231234");
-console.log(field.isPhone());
-
-
+field.addToElement('first');
+field2.addToElement('second');
+field3.addToElement('second');
